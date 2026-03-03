3d0e3fbaa6b50508984292753b21d4ddce09e03b
aly <4ly.alcantara@gmail.com>
2026-03-03 00:41:09 +0000

    feat: update navigation and links in dashboard
    - modified `JAZZMIN_SETTINGS` to add custom link for statistics
    - updated URL paths in `urls.py` for dashboard navigation
    - changed links in `line_chart.html` to point to new dashboard structure

--- config/urls.py
+++ config/urls.py
@@ -3,12 +3,13 @@ from django.urls import path
 from home.views import line_chart, line_chart_json
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('class-board/', admin.site.urls),
-    path('chart/', line_chart, name='line_chart'),
+    path('', RedirectView.as_view(url='/dashboard/', permanent=False)),
+    path('dashboard/', admin.site.urls),
+    path('graficos/', line_chart, name='line_chart'),
     path('chartJSON/', line_chart_json, name='line_chart_json'),
-    
 ]
 
 if settings.DEBUG:
